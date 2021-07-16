df9281dfd8af20d2d9d74ea92945c3cebd8d6441
Benjamin Jasper <jasper.benni@gmail.com>
2021-07-16 11:00:45 +0200

    better slider controls

--- src/app/core/dashboard/slider/slider.component.ts
+++ src/app/core/dashboard/slider/slider.component.ts
@@ -10,12 +10,42 @@ export class SliderComponent implements OnInit {
 
 	index = 0
 	@Input() showButtons = true
+	@Input() listArray?: any[]
 
-	ngOnInit(): void {}
+	previousActive = false
+	nextActive = false
+
+	ngOnInit(): void {
+		if (!this.listArray) {
+			throw Error('Slider needs the array in param listArray that builds the list')
+		}
+		this.checkButtonsActive()
+	}
 
 	public show(event: any, increase: number): void {
 		const liEls: HTMLElement[] = this.elRef.nativeElement.querySelectorAll('li')
 		this.index = (this.index + liEls.length + increase) % liEls.length
+
+		this.checkButtonsActive()
+
 		liEls[this.index].scrollIntoView({ behavior: 'smooth', inline: 'start', block: 'start' })
+	}
+
+	private checkButtonsActive(): void {
+		if (!this.listArray) {
+			throw Error('Slider needs the array in param listArray that builds the list')
+		}
+
+		this.previousActive = true
+		this.nextActive = true
+
+		if (this.index === 0) {
+			this.previousActive = false
+		}
+
+		console.log(this.index)
+		if (this.index === this.listArray.length - 1) {
+			this.nextActive = false
+		}
 	}
 }
