7cd962f265f68e098157f9e2a3d9b43b15a26b6f
Benjamin Jasper <jasper.benni@gmail.com>
2021-09-04 16:18:28 +0200

    feat: new workload element

--- src/app/core/dashboard/circular-progress/circular-progress.component.ts
+++ src/app/core/dashboard/circular-progress/circular-progress.component.ts
@@ -23,30 +23,13 @@ export class CircularProgressComponent implements OnInit, OnChanges, AfterViewIn
 
 	@Input() progress!: number
 	@Input() loading: boolean = false
-	leftBar = 0
-	rightBar = 0
-
+	@Input() radius: number = 80
 	ngOnInit(): void {}
 
 	ngOnChanges(changes: SimpleChanges): void {
-		if (!this.left) {
-			return
-		}
+		console.log(this.progress)
 	}
 
 	ngAfterViewInit(): void {
-		this.evaluateProgress()
-	}
-
-	private evaluateProgress() {
-		if (this.progress <= 50) {
-			const rotation = this.progress * 0.01 * 360
-			this.renderer.setStyle(this.left.nativeElement, 'transform', `rotate(${rotation}deg)`)
-			this.renderer.setStyle(this.right.nativeElement, 'transform', `rotate(0deg)`)
-		} else {
-			const rotation = (this.progress - 50) * 0.01 * 360
-			this.renderer.setStyle(this.left.nativeElement, 'transform', `rotate(180deg)`)
-			this.renderer.setStyle(this.right.nativeElement, 'transform', `rotate(${rotation}deg)`)
-		}
 	}
 }
