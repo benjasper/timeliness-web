96110dcf4d94c98877cc2bc1d3a8b28ddde09e34
Benjamin Jasper <jasper.benni@gmail.com>
2021-09-11 14:06:10 +0200

    fixed font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
 	],
 	darkMode: false,
 	theme: {
+		fontSize: {
+			'xs': '1.2rem',
+			'sm': '1.4rem',
+			'base': '1.6rem',
+			'lg': '1.8rem',
+			'xl': '2rem',
+			'2xl': '2.2rem',
+			'3xl': '2.4rem',
+			'4xl': '2.6rem',
+			'5xl': '2.8rem',
+			'6xl': '3.2rem',
+			'7xl': '3.4rem',
+		},
 		extend: {
 			fontFamily: {
 				signature: ['Karla', ...defaultTheme.fontFamily.sans],
