00823130be84810d807aa64e08eb94b44e386fc2
Benjamin Jasper <jasper.benni@gmail.com>
2021-11-13 18:19:10 +0100

    removed collapsable sidebar

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -9,30 +9,14 @@ import { AuthService } from '../services/auth.service'
 })
 export class CoreComponent implements OnInit {
 	constructor(private authService: AuthService) {}
-
-	public collapsed = false
 	public user: User|undefined = undefined
 
 	ngOnInit(): void {
-		if (localStorage.getItem('dashboard-sidebar') === 'collapsed') {
-			this.collapsed = true
-		}
-
 		this.authService.user.subscribe(user => {
 			if (user) {
 				this.user = user
 			}
 		})
-	}
-
-	public switchSidebarCollapsed(): void {
-		if (this.collapsed) {
-			this.collapsed = false
-		} else {
-			this.collapsed = true
-		}
-
-		localStorage.setItem('dashboard-sidebar', this.collapsed ? 'collapsed' : '')
 	}
 
 	public logout(): void {
