28ea48df0fabbd92c468428ab0579d36c436e3c1
Benjamin Jasper <jasper.benni@gmail.com>
2021-11-27 10:13:17 +0100

    fixed redirect bug on user update

--- src/app/guards/no-onboarding.guard.ts
+++ src/app/guards/no-onboarding.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
+import { first } from 'rxjs/operators'
 import { AuthService } from '../services/auth.service'
 
 @Injectable({
@@ -14,7 +15,7 @@ export class NoOnboardingGuard implements CanActivate {
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 		return new Promise(async (resolve, reject) => {
-			this.authService.user.subscribe((user) => {
+			this.authService.user.pipe(first()).subscribe((user) => {
 				if (user && user.emailVerified && !user.settings.onboardingCompleted) {
 					this.router.navigate(['/onboarding'])
 					resolve(false)
