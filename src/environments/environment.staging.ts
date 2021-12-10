ae008e66996728b0189de882e786c37e05316718
Benjamin Jasper <jasper.benni@gmail.com>
2021-12-10 20:58:30 +0100

    added staging env

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+import { Environment } from './environment-interface'
+
+export const environment: Environment = {
+	production: true,
+	apiBaseUrl: 'https://staging-dot-project-tasks-294214.oa.r.appspot.com',
+}
