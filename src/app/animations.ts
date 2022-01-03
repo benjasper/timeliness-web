b88432aee8de5985e71607369e175bf7b263b4b9
Benjamin Jasper <jasper.benni@gmail.com>
2022-01-03 19:04:48 +0100

    improved transition

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,6 @@
-import { animate, group, query, style, transition, trigger } from '@angular/animations'
+import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations'
+
+const optional = { optional: true }
 
 export const smoothHeight = trigger('grow', [
 	transition('void <=> *', []),
@@ -7,17 +9,46 @@ export const smoothHeight = trigger('grow', [
 	}),
 ])
 
-export const sliderRoutes =
-  trigger('routeAnimations', [
-    transition('dashboard => *', slideTo('bottom') ),
-    transition('agenda => dashboard', slideTo('top') ),
-    transition('agenda => settings', slideTo('bottom') ),
-    transition('settings => *', slideTo('top') ),
-    transition('* => *', slideTo('top') ),
-  ]);
+export const sliderRoutes = trigger('routeAnimations', [
+	transition('dashboard => *', slideTo('bottom')),
+	transition('agenda => dashboard', slideTo('top')),
+	transition('agenda => settings', slideTo('bottom')),
+	transition('settings => *', slideTo('top')),
+	transition('* <=> *', [
+		// Set a default  style for enter and leave
+		query(':enter, :leave', [
+			style({
+				position: 'absolute',
+				left: 0,
+				width: '100%',
+				opacity: 0,
+				//transform: 'scale(3)',
+			}),
+		]),
+		// Animate the new page in
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1)' }))]),
+	]),
+])
+
+export const fader = trigger('routeAnimations', [
+	transition('* <=> *', [
+		// Set a default  style for enter and leave
+		query(':enter, :leave', [
+			style({
+				position: 'absolute',
+				left: 0,
+				width: '100%',
+				opacity: 0,
+				transform: 'scale(0) translateY(100%)',
+			}),
+		]),
+		
+		// Animate the new page in
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' }))]),
+	]),
+])
 
 function slideTo(direction: string) {
-	const optional = { optional: true }
 	return [
 		query(
 			':enter, :leave',
@@ -25,7 +56,7 @@ function slideTo(direction: string) {
 				style({
 					position: 'absolute',
 					left: '50%',
-    				transform: 'translateX(-50%)',
+					transform: 'translateX(-50%)',
 					[direction]: 0,
 					width: '100%',
 				}),
