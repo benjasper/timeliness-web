f90608a6a4ef6ca27e7d34cbe46aa5d95c2796a5
Benjamin Jasper <jasper.benni@gmail.com>
2022-02-06 21:01:34 +0100

    added better allround animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -24,25 +24,31 @@ export const sliderRoutes = trigger('routeAnimations', [
 			}),
 		]),
 		// Animate the new page in
-		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1)' }))], {optional: true}),
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1)' }))], { optional: true }),
 	]),
 ])
 
 export const fader = trigger('routeAnimations', [
 	transition('* <=> *', [
 		// Set a default  style for enter and leave
-		query(':enter, :leave', [
-			style({
-				position: 'absolute',
-				left: 0,
-				width: '100%',
-				opacity: 0,
-			}),
-		], {optional: true}),
-		
+		query(
+			':enter, :leave',
+			[
+				style({
+					position: 'absolute',
+					left: 0,
+					width: '100%',
+					opacity: 0,
+				}),
+			],
+			{ optional: true }
+		),
+
 		// Animate the new page in
-		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' }))], {optional: true}),
-		animateChild()
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' }))], {
+			optional: true,
+		}),
+		animateChild(),
 	]),
 ])
 
