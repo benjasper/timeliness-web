5ee2e3c18389635f5615ff75c0f4074a91bb3137
Benjamin Jasper <jasper.benni@gmail.com>
2022-02-06 21:05:10 +0100

    Better route transitions (#39)
    * added better allround animations
    
    * added better allround animations
    
    * added better allround animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -10,10 +10,8 @@ export const smoothHeight = trigger('grow', [
 ])
 
 export const sliderRoutes = trigger('routeAnimations', [
-	transition('dashboard => *', slideTo('bottom')),
-	transition('agenda => dashboard', slideTo('top')),
-	transition('agenda => settings', slideTo('bottom')),
-	transition('settings => *', slideTo('top')),
+	transition(':increment', slideTo('bottom')),
+	transition(':decrement', slideTo('top')),
 	transition('* <=> *', [
 		// Set a default  style for enter and leave
 		query(':enter, :leave', [
@@ -26,25 +24,31 @@ export const sliderRoutes = trigger('routeAnimations', [
 			}),
 		]),
 		// Animate the new page in
-		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1)' }))]),
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1)' }))], { optional: true }),
 	]),
 ])
 
 export const fader = trigger('routeAnimations', [
 	transition('* <=> *', [
 		// Set a default  style for enter and leave
-		query(':enter, :leave', [
-			style({
-				position: 'absolute',
-				left: 0,
-				width: '100%',
-				opacity: 0,
-				transform: 'scale(0) translateY(100%)',
-			}),
-		]),
-		
+		query(
+			':enter, :leave',
+			[
+				style({
+					position: 'absolute',
+					left: 0,
+					width: '100%',
+					opacity: 0,
+				}),
+			],
+			{ optional: true }
+		),
+
 		// Animate the new page in
-		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' }))]),
+		query(':enter', [animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' }))], {
+			optional: true,
+		}),
+		animateChild(),
 	]),
 ])
 
